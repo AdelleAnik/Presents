99495ad1b2ccf0a16d9506fde250c80bac04700d
Adelle Gomes <adelle-anik@hotmail.com>
2024-08-12 17:04:18 -0300

    mved all the data to hasura-graphql

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import './Homepage.css';
 import CardList from "../Components/CardsList";
+import { useQuery, gql } from '@apollo/client';
 
-// Dummy data
-const items = [
-    { id: 1, name: 'Clear Sparkling Crown Ring', price:'CAD$ 100' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/clear-sparkling-crown-solitaire-ring/198289CZ.html#cgid=rings&sz=20', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488438/198289CZ_RGB_d3ggvb.jpg' },
-    { id: 2, name: 'Square Sparkle Halo Ring', price:'CAD$ 100' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/square-sparkle-halo-ring/198863C01.html', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488608/198863C01_RGB_auizbk.jpg' },
-    { id: 3, name: 'Shooting Stars Sparkling Ring', price:'CAD$ 55' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/stackable-rings/shooting-stars-sparkling-ring/192365C01.html', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488676/198863C01_RGB_u8ycdr.jpg' },
-    { id: 4, name: 'Clear Heart Solitaire Ring', price:'CAD$ 65' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/clear-heart-solitaire-ring/198691C01.html', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488758/198863C01_RGB_sp3qqp.jpg' },
-    { id: 5, name: 'Tiara Wishbone Ring', price:'CAD$ 55', description: 'Latest tech gadget that...', url:'https://ca.pandora.net/en/rings/rings/stackable-rings/tiara-wishbone-ring/198282CZ.html#navigation', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723489031/198863C01_RGB_kn7z39.jpg' },
-  ];
+const GET_PRESENTS = gql`
+  query GetPresents {
+  presents {
+    category
+    description
+    image_url
+    name
+    price
+    url
+  }
+}
+`;
 
 
 function Homepage(){
+    const { loading, error, data } = useQuery(GET_PRESENTS);
+    console.log(data.presents)
+    if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>;
     return(
         <div>
            <h1 style={{ textAlign: 'center' }}>Gifts for Adelle</h1>
-           <CardList items={items} />
+           <CardList items={data.presents} />
         </div>
     )
 }
---
+++ src/apolloClient.js
@@ -0,0 +1,15 @@
+import { ApolloClient, InMemoryCache, HttpLink, ApolloProvider } from '@apollo/client';
+
+const httpLink = new HttpLink({
+  uri: 'https://internal-cattle-16.hasura.app/v1/graphql',
+  headers: {
+    'x-hasura-admin-secret': '5HMIJ3RMzyUU61rgnRbpbcMnSPC5kSYwHr8S8bGDv4tk7ZLC8A85IY6H6r3PBm9C'  // Use environment variables in production
+  }
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache()
+});
+
+export default client;
