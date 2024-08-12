f5d5b8fc393e4da60ac1cfd8fc42a0b2fd067993
Adelle Gomes <adelle-anik@hotmail.com>
2024-08-12 16:28:59 -0300

    updated background color

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import './Homepage.css';
 import CardList from "../Components/CardsList";
 
 // Dummy data
 const items = [
-    { id: 1, name: 'Clear Sparkling Crown Solitaire Ring', price:'CAD$ 100' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/clear-sparkling-crown-solitaire-ring/198289CZ.html#cgid=rings&sz=20', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488438/198289CZ_RGB_d3ggvb.jpg' },
+    { id: 1, name: 'Clear Sparkling Crown Ring', price:'CAD$ 100' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/clear-sparkling-crown-solitaire-ring/198289CZ.html#cgid=rings&sz=20', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488438/198289CZ_RGB_d3ggvb.jpg' },
     { id: 2, name: 'Square Sparkle Halo Ring', price:'CAD$ 100' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/square-sparkle-halo-ring/198863C01.html', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488608/198863C01_RGB_auizbk.jpg' },
     { id: 3, name: 'Shooting Stars Sparkling Ring', price:'CAD$ 55' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/stackable-rings/shooting-stars-sparkling-ring/192365C01.html', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488676/198863C01_RGB_u8ycdr.jpg' },
     { id: 4, name: 'Clear Heart Solitaire Ring', price:'CAD$ 65' ,description: 'Size 5', url:'https://ca.pandora.net/en/rings/rings/promise-rings/clear-heart-solitaire-ring/198691C01.html', imageUrl: 'https://res.cloudinary.com/dfn11pasm/image/upload/v1723488758/198863C01_RGB_sp3qqp.jpg' },
